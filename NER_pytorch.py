111664cb6a5f1c447122b96d1f98f84c53bcc963
sagun-shakya <sagun.shakya@naamii.org.np>
2021-10-27 11:43:07 +0545

    Implemented Load_dataset module. Accuracy problem solved somehow.

--- NER_pytorch.py
+++ NER_pytorch.py
@@ -7,52 +7,32 @@ Created on Thu Oct 21 14:31:14 2021
 
 # Importing necessary libraries.
 import matplotlib.pyplot as plt
-import pandas as pd
-import numpy as np
-import os
-np.random.seed(0)
 plt.style.use("ggplot")
 
 from sklearn.model_selection import train_test_split
 
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
-
 from torch.optim import Adam
 from torch.utils.data import DataLoader
-
-#from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
-#from keras.preprocessing.sequence import pad_sequences
 
 # LOCAL MODULES.
 import utils
 from BiLSTM_module import BilSTM_model
 from Dataset_module import NERDataset
 from EarlyStopping_module import EarlyStopping
+from Load_dataset import LoadDataset
 
 # Filter out warning messages.
 from warnings import filterwarnings
 filterwarnings('ignore')
 
-# Parsing data.
-data_path = r'D:\ML_projects\NER_pytorch_english\data'
-filename = os.path.join(data_path, 'ner_dataset.csv')
-
-data = pd.read_csv(filename, encoding="latin1")
-data = data.fillna(method="ffill")
-
-# Creating a list of unique words and tags.
-words = data['Word'].unique().tolist()
-words = words + ['<UNK>', '<PAD>', 'ENDTAG']
-tags = data['Tag'].unique().tolist()
-
+#################### CONSTANTS ####################
 # Global constants.
 TEST_SIZE = 0.2
 RANDOM_STATE = 1
 
 # For NN.
-VOCAB_SIZE = len(words)
 MAX_LEN = 50
 EMBEDDING_DIM = 50
 HIDDEN_DIM = 100
@@ -60,36 +40,53 @@ DROPOUT_PROB = 0.1
 BATCH_SIZE = 8
 EPOCHS = 3
 PATIENCE = 5
+#-------------------------------------------------#
+
+#################### VARIABLE INITIALIZATION ####################
+# Root and filename.
+data_path = r'D:\ML_projects\NER_pytorch_english\data'
+filename = 'ner_dataset.csv'
+
+# Main Dataholder class.
+main = LoadDataset(data_path, filename)
+
+# Unique words.
+words = main.words
+
+# Size of the vocabulary.
+VOCAB_SIZE = len(words)
+
+# Unique tags.
+tags = main.tags
+
+# Number of output classes.
 num_tags = len(tags)
 
-# Aggregation function to make a list of tokens/tags in each row.
-agg_function_word_only = lambda df: [w for w in df['Word'].values.tolist()]
-agg_function_tag_only = lambda df: [t for t in df['Tag'].values.tolist()]
+# Sentences. List of word_lists.
+sentences = main.sentences
 
-grouped = data.groupby("Sentence #")
+# Tag Sequence.
+tag_sequence = main.tag_sequence
 
-# List of word_lists and tag_tokens.
-sentences = grouped.apply(agg_function_word_only).tolist()
-tag_sequence = grouped.apply(agg_function_tag_only).tolist() 
+# Seq2idx.
+word2idx = main.word2idx
+tag2idx = main.tag2idx
 
-# Mappings from word/tag to its IDs.
-word2idx = {word : ii for ii, word in enumerate(words, 1)}
-tag2idx = {tag : ii for ii, tag in enumerate(tags, 0)}
-tag2idx['<pad>'] = 17
+# Idx2seq.
+idx2word = main.idx2word
+idx2tag = main.idx2tag
 
-# Inverse mapping.
-idx2word = {ii : word for ii, word in enumerate(words, 1)}
-idx2tag = {ii : tag for ii, tag in tag2idx.items()}
-idx2tag[17] = '<pad>'
+# Pad_id.
+pad_id = main.word_pad_id
+tag_pad_id = main.tag_pad_id
 
-# ID for <PAD> in a sentence.
-pad_id = word2idx['<PAD>']
 
-# List of token IDs for wach sentence and the tagset. 
-unpadded_sequences = [utils.word_list2id_list(SEQ, word2idx, tag2idx, 'word') for SEQ in sentences]
+
+# Unpadded sequences and tags.
+unpadded_sequences = main.unpadded_sequences
 unpadded_sequences = list(map(torch.tensor, unpadded_sequences))
 
-unpadded_tags = [utils.word_list2id_list(SEQ, word2idx, tag2idx, 'tag') for SEQ in tag_sequence]
+unpadded_tags = main.unpadded_tags
 unpadded_tags = list(map(torch.tensor, unpadded_tags))
 
 # Padding the sentences with the ID for <PAD> and tags with the ID for <pad>.
@@ -104,7 +101,9 @@ x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_
 # Creating training and testing iterators.
 train_loader = DataLoader(NERDataset(x_train, y_train, pad_id), batch_size = BATCH_SIZE, shuffle=True)
 test_loader = DataLoader(NERDataset(x_test, y_test, pad_id), batch_size = BATCH_SIZE, shuffle=True)
+#---------------------------------------------------------------#
 
+#################### MODEL BUILDING ####################
 # Model Architecture.
 model = BilSTM_model(VOCAB_SIZE, EMBEDDING_DIM, HIDDEN_DIM, DROPOUT_PROB, MAX_LEN, num_tags, BATCH_SIZE)
 
@@ -151,7 +150,8 @@ for ee in range(EPOCHS):
         # Preventing the <pad> element from contributing to the loss.
         loss = F.nll_loss(preds, tag, ignore_index = tag2idx['<pad>'])
         loss_rounded = round(loss.item(), 4)
-        #print(f"\nLoss value in this iteration (Epoch : {ee} & Batch : {ii}): {loss_rounded}")
+        
+        #print(f"\nLoss value in this iteration (Epoch : {ee} & Batch : {ii}): {loss_rounded}")    # For debugging.
 
         # Backpropagation.
         loss.backward()
@@ -161,7 +161,7 @@ for ee in range(EPOCHS):
 
         # Categorical Accuracy.
         train_acc_per_iter = utils.categorical_accuracy(preds, tag, tag_pad_value = tag2idx['<pad>'])
-        #print(f"Accuracy in this iteration (Epoch : {ee} & Batch : {ii}): {train_acc_per_iter}")
+        #print(f"Accuracy in this iteration (Epoch : {ee} & Batch : {ii}): {train_acc_per_iter}")    # For debugging.
 
         # Calculate the loss and accuracy for the validation set in every 50 iteration.
         if (ii + 1) % 50 == 0:
@@ -222,4 +222,4 @@ for ee in range(EPOCHS):
         break
     
     break
-    
+ #---------------------------------------------------------------#   
